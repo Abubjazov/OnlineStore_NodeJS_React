9193b037c642ec8084ffdb90a23dde16f696c70f
Radik Abubjazov <abubjazov@mail.ru>
2022-01-07 22:48:36 +0300

    server: /route: /user/auth +

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,6 +2,9 @@ import { Router } from 'express'
 
 export const userRouter = new Router()
 
-userRouter.get('/auth', )
+userRouter.get('/auth', (req, res) => {
+    res.json({ message: 'It is OK! From /user/auth' })
+})
+
 userRouter.post('/registration', )
 userRouter.post('/login', )
