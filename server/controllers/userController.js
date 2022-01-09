f06b31a3ec19273393572a29660b084932a24992
Radik Abubjazov <abubjazov@mail.ru>
2022-01-09 23:36:21 +0300

    server: class UserController: +registration()

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,32 @@
+import bcrypt from 'bcrypt'
+import Jwt  from 'jsonwebtoken'
+
+
 import { ApiError } from '../error/ApiError.js'
+import { Basket, User } from '../models/models.js'
 
 export class UserController {
-    async registration(req, res) {
+    async registration(req, res, next) {
+        const { email, password, role } = req.body
+        if (!email && !password) {
+            return next(ApiError.badRequest('Incorrect email or password'))
+        }
 
+        const candidate = await User.findOne({ where: { email } })
+        if (candidate) {
+            return next(ApiError.badRequest(`User with email: ${ email } already exists`))
+        }
+
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({ email, role, password: hashPassword })
+        const basket = await Basket.create({ userId: user.id })
+        const token = Jwt.sign(
+            { id: user.id, email, role}, 
+            process.env.SECRET_KEY,
+            {expiresIn: '8h'}
+        )
+
+        return res.json({ token })
     }
 
     async login(req, res) {
