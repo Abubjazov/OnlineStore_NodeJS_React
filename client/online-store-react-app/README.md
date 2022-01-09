63406211b5747ead57233cf80121e03806a8cbb9
Radik Abubjazov <abubjazov@mail.ru>
2022-01-10 01:26:28 +0300

    client: Start point

---
+++ client/online-store-react-app/README.md
@@ -0,0 +1,3 @@
+# Online Store
+
+##  Online Store is a React-based single page application designed to work with the REST API based backEnd made with Node.js.
---
+++ server/utils/index.js
@@ -0,0 +1,45 @@
+import 'dotenv/config'
+import express from 'express'
+import { sequelize } from './database.js'
+import cors from 'cors' 
+import fileUpload from 'express-fileupload'
+
+import { router } from '../routes/index.js'
+import { errorHandler } from '../middleware/ErrorHandler.js'
+import {
+  User,
+  Basket,
+  BasketDevice,
+  Device,
+  Type,
+  Brand,
+  Rating,
+  TypeBrand,
+  DeviceInfo
+} from '../models/models.js'
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('static'))
+app.use(fileUpload({}))
+app.use('/api', router)
+
+app.use(errorHandler) //Always the last Middleware!
+
+const start = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync() //{force: true}
+    
+    app.listen(process.env.PORT, () =>
+      console.log(`Server listen PORT: ${process.env.PORT}`)
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
