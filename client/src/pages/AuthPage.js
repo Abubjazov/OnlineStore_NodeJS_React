958b0ddcf3055bf45c5ef3588ab526025a6bf5d4
Radik Abubjazov <abubjazov@mail.ru>
2022-01-12 00:03:19 +0300

    client: pages: AuthPage+

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Card, Container, Form, FormControl, Button, Row, Col } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
-import { REGISTRATION_ROUTE } from '../utils/consts'
+import { NavLink, useLocation } from 'react-router-dom'
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts'
 
 export const AuthPage = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
+
     return (
         <Container
             className='d-flex justify-content-center align-items-center'
             style={{ height: window.innerHeight - 54 }}
         >
             <Card style={{ width: 600 }} className='p-5'>
-                <h2 className='m-auto'>LogIn</h2>
+                <h2 className='m-auto'>{isLogin ? 'LogIn' : 'Registration'}</h2>
                 <Form className='d-flex flex-column '>
                     <FormControl
                         className='mt-3'
@@ -24,11 +27,11 @@ export const AuthPage = () => {
                         <Col>
                             <Button
                                 variant='outline-success'
-                            >LogIn</Button>
+                            >{isLogin ? 'LogIn' : 'SignUp'}</Button>
                         </Col>
                         <Col>
                             <div>
-                                Don't have an account? <NavLink to={REGISTRATION_ROUTE}>Sign up!</NavLink>
+                                {isLogin ? `Don't have an account?` : 'Have an account?'} <NavLink to={isLogin ? REGISTRATION_ROUTE : LOGIN_ROUTE}>{isLogin ? 'SignUp' : 'LogIn'}</NavLink>
                             </div>
                         </Col>
                     </Row>
