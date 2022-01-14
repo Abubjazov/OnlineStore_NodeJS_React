1ce217db0876647471a92efb870f2b9c8b752e0b
Radik Abubjazov <abubjazov@mail.ru>
2022-01-14 09:06:03 +0300

    client: components: modals: CreateDevice+

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import { Button, Form, Modal } from 'react-bootstrap'
+import React, { useContext } from 'react'
+import { Button, Dropdown, Form, Modal } from 'react-bootstrap'
+import { Context } from '../..'
 
 export const CreateDevice = ({ show, onHide }) => {
+    const { device } = useContext(Context)
+
     return (
         <Modal
             show={show}
@@ -17,8 +20,42 @@ export const CreateDevice = ({ show, onHide }) => {
 
             <Modal.Body>
                 <Form>
+                    <div className='d-flex'>
+                        <Dropdown className='m-2'>
+                            <Dropdown.Toggle variant="secondary">Select type</Dropdown.Toggle>
+                            <Dropdown.Menu variant="dark">
+                                {device.types.map(type =>
+                                    <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+                                )}
+                            </Dropdown.Menu>
+                        </Dropdown>
+
+                        <Dropdown className='m-2'>
+                            <Dropdown.Toggle variant="secondary">Select brand</Dropdown.Toggle>
+                            <Dropdown.Menu variant="dark">
+                                {device.brands.map(brand =>
+                                    <Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>
+                                )}
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+
+                    <hr />
+
                     <Form.Control
+                        className='mt-3'
                         placeholder={'Input device name'}
+                    />
+
+                    <Form.Control
+                        className='mt-3'
+                        placeholder={'Input device price'}
+                        type='number'
+                    />
+
+                    <Form.Control
+                        className='mt-3'
+                        type='file'
                     />
                 </Form>
             </Modal.Body>
