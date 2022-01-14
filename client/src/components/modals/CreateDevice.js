69bfcb4746d7669dfc85d42e7814ee96d4b8f905
Radik Abubjazov <abubjazov@mail.ru>
2022-01-14 09:26:51 +0300

    client: components: modals: CreateDevice+

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,9 +1,19 @@
-import React, { useContext } from 'react'
-import { Button, Dropdown, Form, Modal } from 'react-bootstrap'
+import React, { useContext, useState } from 'react'
+import { Button, Col, Dropdown, Form, Modal, Row } from 'react-bootstrap'
+import { nanoid } from 'nanoid'
 import { Context } from '../..'
 
 export const CreateDevice = ({ show, onHide }) => {
     const { device } = useContext(Context)
+    const [info, setInfo] = useState([])
+
+    const addInfo = () => {
+        setInfo([...info, { id: nanoid(), title: '', desc: '' }])
+    }
+
+    const removeInfo = (id) => {
+        setInfo([...info].filter(item => item.id !== id))
+    }
 
     return (
         <Modal
@@ -57,6 +67,31 @@ export const CreateDevice = ({ show, onHide }) => {
                         className='mt-3'
                         type='file'
                     />
+
+                    <hr />
+
+                    <Button variant='outline-dark' onClick={addInfo}>Add New Property</Button>
+                    {info.map(row =>
+                        <Row key={row.id}>
+                            <Col md={4}>
+                                <Form.Control
+                                    className='mt-3'
+                                    placeholder={'Input property name'}
+                                />
+                            </Col>
+
+                            <Col md={4}>
+                                <Form.Control
+                                    className='mt-3'
+                                    placeholder={'Input property description'}
+                                />
+                            </Col>
+
+                            <Col md={4}>
+                                <Button variant='outline-danger' className='mt-3' onClick={() => removeInfo(row.id)}>Remove</Button>
+                            </Col>
+                        </Row>
+                    )}
                 </Form>
             </Modal.Body>
 
